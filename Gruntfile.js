ba1b2b6bdd745ea399a91c012aa8f19e6873f550
Stephane Fidanza <stephane.fidanza@gmail.com>
2014-05-25 16:38:15 +0200

    Use a target folder for the build, and route nginx statics to it. !!Requires changes to nginx configuration!!

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,26 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		clean: ["target"],
+		copy: {
+			build: { expand: true, cwd: 'src/client/', src: [ 'img/**' ], dest: 'target/static/' },
+		},
 		concat: {
 			build: grunt.file.readJSON('build/cfg/concat.json')
 		},
 		mergeTemplates: {
 			build: grunt.file.readJSON('build/cfg/mergeTemplates.json')
+		},
+		cssmin: {
+			minify: { expand: true, cwd: 'target/static/', src: [ '*.css', '!*.min.css' ], dest: 'target/static/', ext: '.min.css' }
+		},
+		uglify: {
+			minify: {
+				files: {
+					'target/static/frw.min.js': 'target/static/frw.js',
+					'target/static/app.min.js': 'target/static/app.js',
+				}
+			}
 		},
 		jshint: {
 			options: {
@@ -15,15 +30,34 @@ module.exports = function(grunt) {
 			},
 			client: ['src/client/*/*.js'],
 			server: ['src/server/**/*.js']
-		}
+		},
+		watch: {
+			jscss: {
+				files: [ 'src/client/**/*.js', 'src/client/**/*.css' ],
+				tasks: [ 'concat' ]
+			},
+			tpl: {
+				files: 'src/client/**/*.html',
+				tasks: [ 'mergeTemplates' ]
+			},
+			img: {
+				files: 'src/client/img/**',
+				tasks: [ 'copy' ]
+			}
+		},
 	});
 	
 	// Load plugins
-	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadTasks('build/tasks');
 	
 	// Default tasks
-	grunt.registerTask('default', [ 'jshint', 'concat', 'mergeTemplates' ]);
-	
+	grunt.registerTask('default', [ 'jshint', 'clean', 'copy', 'concat', 'mergeTemplates' ]);
+	grunt.registerTask('minify', [ 'cssmin', 'uglify' ]);
 };
--- README.md
+++ README.md
@@ -3,11 +3,11 @@ worldcup2014
 
 A site to follow the calendar and results of the 2014 worldcup
 
-The site is using a node.js backend to retrieve data, and a javascript frontend to display them. It is as much a functional interest, to follow the worldcup, as a technical one, to play with technologies.
+The site is using a node.js backend to retrieve data, and a javascript frontend to display them. It is as much
+a functional interest, to follow the worldcup, as a technical one, to play with technologies.
 
 Functionally, the site has:
 - the list of matches, locations, date/hours (France time)
-- the results
 - the groups schedule and results
 - a final board to follow the round of 16 to the final
 
@@ -15,5 +15,10 @@ Technically:
 - the js frontend uses a client-side template engine to display the views
 - page navigation and history is handled by the js frw layer
 - data is retrieved in ajax targeting the backend api
-- the backend goes through nginx, proxying to node.js (todo: have nginx handle the static requests itself)
+- the server is handled by nginx, serving the statics and proxying the api calls to node.js
 - for now, data is stored in json files (todo: use mongodb for storage, for easy online edition)
+
+Development:
+- 'grunt' runs jshint, then cleans and rebuild the target static folder (js/css are not minified)
+- 'grunt minify' creates the minifed js/css files (not used for now, just to test)
+- 'grunt watch' watches for any change on client files (img, js, css, html) to update the target folder on the fly
