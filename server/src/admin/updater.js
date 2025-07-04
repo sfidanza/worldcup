aac9f666645f772f1f4f4b5935fcc4a399476abd
Stephane Fidanza <stephane.fidanza@gmail.com>
2025-07-04 22:46:28 +0200

    feat: add admin endpoint to fetch live score updates (experimental)
    Note: combined with live mode from client, watchers will see the live updates.

---
+++ server/src/admin/updater.js
@@ -0,0 +1,48 @@
+/********************************************************************
+ * Update matches data from live api
+ ********************************************************************/
+const updater = {};
+export default updater;
+
+const LIVE_API = 'https://api.fifa.com/api/v3/live/football/';
+
+/********************************************************************
+ * Update data
+ */
+
+updater.fetch = async function (db, mid) {
+	return fetch(LIVE_API + mid)
+		.then(res => res.json())
+		.then(data => {
+			return {
+				date: data.Date,
+				day: data.Date.slice(0, 10), // raw but should work for now
+				matchTime: data.MatchTime,
+				winner: data.Winner,
+				team1: data.HomeTeam.Abbreviation,
+				score1: data.HomeTeam.Score,
+				team2: data.AwayTeam.Abbreviation,
+				score2: data.AwayTeam.Score,
+				score1PK: data.HomeTeamPenaltyScore,
+				score2PK: data.AwayTeamPenaltyScore
+			};
+		})
+		.then(match => {
+			const edit = {
+				'team1_score': match.score1,
+				'team2_score': match.score2,
+				'team1_scorePK': match.score1PK,
+				'team2_scorePK': match.score2PK
+			};
+			return db.collection('matches')
+				.findOneAndUpdate(
+					{ team1_id: match.team1, team2_id: match.team2 },
+					{ $set: edit },
+					{ returnDocument: 'after' }
+				)
+				.then(updated => {
+					updated.matchTime = match.matchTime;
+					return updated;
+				});
+		});
+};
--- server/src/routers/admin.js
+++ server/src/routers/admin.js
@@ -5,9 +5,12 @@
  *   /api/<year>/admin/preview -> preview the data from the filesystem
  *   /api/<year>/admin/drop    -> drop the database
  *   /api/<year>/admin/import  -> import the filesystem data in database
+ *   /api/<year>/update?mid=xx -> update the match score (mid is the FIFA match id)
  ******************************************************************************/
 import { Router } from 'express';
 import importer from '../admin/importer.js';
+import updater from '../admin/updater.js';
+import live from '../business/live.js';
 
 export default function getRouter() {
 	const router = Router();
@@ -45,6 +48,21 @@ export default function getRouter() {
 			importer.import(db)
 				.then(data => {
 					response.json(data);
+				}).catch(err => response.status(err.statusCode ?? 500).json({ error: err.message }));
+		} else {
+			response.status(401).json({ error: 'Unauthorized' });
+		}
+	});
+
+	router.get('/update', function (request, response) {
+		const user = request.session.user;
+		if (user && user.isAdmin) {
+			const query = request.query;
+			const db = request.database;
+			updater.fetch(db, query.mid)
+				.then(match => {
+					live.broadcastMatchUpdate(match);
+					response.json(match);
 				}).catch(err => response.status(err.statusCode ?? 500).json({ error: err.message }));
 		} else {
 			response.status(401).json({ error: 'Unauthorized' });
