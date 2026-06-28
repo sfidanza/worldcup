d4b2b68c2eb887496ea01f6eb4dea53dd20cb361
Stephane Fidanza <stephane.fidanza@gmail.com>
2026-06-28 16:53:53 +0200

    fix: improve best thirds computation to categorize every team

--- client/src/templates/bestThirds.js
+++ client/src/templates/bestThirds.js
@@ -21,13 +21,17 @@ bestThirds.onParse = function (teams) {
 
 	const stillPlaying = bestThirds.filter(t => t.played < 3).length;
 	const minQualifyingPoints = bestThirds[QUALIFIED_COUNT - 1].points;
+	let playedAll = 0;
 
 	this.set('flag', page.config.cid === 'cwc' ? 'club' : 'flag');
 	bestThirds.forEach((team, i) => {
 		const highlighted = (team.played < 3) ? ' highlighted'      // still playing, points can only increase, goal_diff can decrease
 			: (i < QUALIFIED_COUNT - stillPlaying) ? ' qualified'   // not enough still playing teams to push them below the qualifying line
-			: (team.points < minQualifyingPoints) ? ' eliminated' // not enough points to qualify
+			: (playedAll >= QUALIFIED_COUNT || team.points < minQualifyingPoints) ? ' eliminated' // not enough points to qualify
 			: '';
+		if (team.played === 3) {
+			playedAll++;
+		}
 
 		this.set('row_class', 'l' + (i % 2));
 		this.set('highlighted', highlighted);
